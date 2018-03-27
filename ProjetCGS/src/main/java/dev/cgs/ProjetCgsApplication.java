815bf23b5c445e3b35a299c48fc4575346290b37
bamba <6ccattleya@gmail.com>
2018-03-27 23:22:14 +0000

    Inialisation du projet -> projet vide avec les dependances: web, thymeleaf, security, JPA, MYSQL.

---
+++ ProjetCGS/src/main/java/dev/cgs/ProjetCgsApplication.java
@@ -0,0 +1,12 @@
+package dev.cgs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetCgsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetCgsApplication.class, args);
+	}
+}
